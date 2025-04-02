2020ecd6a7035cfbc22984fb5b17463b9d15bdc4
cristobalmestre <cristobal.mestre@gmail.com>
2025-04-02 14:49:30 -0400

    Ranpac to device

--- models/ranpac_original.py
+++ models/ranpac_original.py
@@ -113,7 +113,7 @@ class Learner(BaseLearner):
 
         ridge = 100000
 
-        Wo = torch.linalg.solve(self.G + ridge*torch.eye(self.G.size(dim=0)), self.Q).T # better nmerical stability than .invv
+        Wo = torch.linalg.solve(self.G + ridge*torch.eye(self.G.size(dim=0)), self.Q).T.to(self._device) # better nmerical stability than .invv
         self._network.fc.weight.data = Wo[0:self._network.fc.weight.shape[0], :].to(self._device)
 
         # print(self._network.fc.weight.data.shape)
